6a2b65f9f781b88d9fd8969a3df52d4138ecc83b
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2021-12-23 13:41:57 +0500

    7

--- лаба ооп4.2/Form1.Designer.cs
+++ лаба ооп4.2/Form1.Designer.cs
@@ -146,7 +146,6 @@ namespace лаба_ооп4._2
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
